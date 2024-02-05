b43198968ceaadb0dde57dcac41455e316851319
Ludovic <34213508+B4L00@users.noreply.github.com>
2024-02-05 14:55:39 +0100

    Add files via upload

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: larmenou <larmenou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 11:34:09 by larmenou          #+#    #+#             */
-/*   Updated: 2024/02/05 10:46:27 by larmenou         ###   ########.fr       */
+/*   Updated: 2024/02/05 14:53:56 by larmenou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,15 @@ ScalarConverter::~ScalarConverter()
 
 void ScalarConverter::convert(std::string str)
 {
-	int 			i = 0;
-	double			d = 0.0;
-	char			c = 0;
-	float			f = 0.0f;
-	unsigned int	j = 0;
-	int 			cpt_dot = 0;
-	int 			cpt_f = 0;
+	int 				i = 0;
+	double				d = 0.0;
+	char				c = 0;
+	float				f = 0.0f;
+	unsigned int		j = 0;
+	int 				cpt_dot = 0;
+	int 				cpt_f = 0;
+	int					cpt_plus_minus = 0;
+	std::stringstream	ss;
  
 	if (str.length() == 1 && isprint(str[0]) && !isdigit(str[0]))
 	{
@@ -49,21 +51,11 @@ void ScalarConverter::convert(std::string str)
 		return ;
 	}
 
-	/* 
-		Probleme avec :
-		f42. ok
-		42. ok
-		42.f ok
-		+42-
-		+++42
-		---42
-	*/
-
 	while (str[j])
 	{
-		if ((!isdigit(str[j]) && (str[j] != '.' && str[j] != 'f' && str[j] != '-' && str[j] != '+'))
+		if ((!isdigit(str[j]) && (str[j] != '.' && str[j] != 'f' && str[j] != '-' && str[j] != '+' && str[j] != 'e'))
 			|| (str[j] == 'f' && j != str.length() - 1)
-			|| (j != 0 && str[j] == 'f' && !isdigit(str[j-1]))
+			|| (j != 0 && str[j] == 'f' && !isdigit(str[j - 1]))
 			|| (str[j] == '.' && (j == str.length() - 1 || j == 0)))
 		{
 			std::cout << "char: impossible" << std::endl;
@@ -76,9 +68,12 @@ void ScalarConverter::convert(std::string str)
 			cpt_dot ++;
 		else if (str[j] == 'f' && j == str.length() - 1)
 			cpt_f ++;
+		else if (str[j] == '-' || str[j] == '+')
+			cpt_plus_minus ++;
 		j++;
 	}
-	if (cpt_f == 1 && cpt_dot == 1)
+
+	if (cpt_f == 1 && cpt_dot == 1 && cpt_plus_minus < 2)
 	{
 		f = atof(str.c_str());
 		c = static_cast<char>(f);
@@ -91,7 +86,8 @@ void ScalarConverter::convert(std::string str)
 		std::cout << "double: " << static_cast<double>(f) << ".0" << std::endl;
 		return ;
 	}
-	else if (cpt_dot == 1 && cpt_f == 0)
+	
+	else if (cpt_dot == 1 && cpt_f == 0 && cpt_plus_minus < 2)
 	{
 		d = strtod(str.c_str(), NULL);
 		c = static_cast<char>(d);
@@ -104,9 +100,12 @@ void ScalarConverter::convert(std::string str)
 		std::cout << "double: " << d << ".0" << std::endl;
 		return ;
 	}
-	else if (cpt_dot == 0 && cpt_f == 0)
+	
+	else if (cpt_dot == 0 && cpt_f == 0 && cpt_plus_minus < 2)
 	{
-		i = atoi(str.c_str());
+		//i = atoi(str.c_str());
+		ss << str;
+		ss >> i;
 		c = static_cast<char>(i);
 		if (c && c > 0)
 			std::cout << "char: '" << c << "'" << std::endl;
@@ -117,6 +116,7 @@ void ScalarConverter::convert(std::string str)
 		std::cout << "double: " << static_cast<double>(i) << ".0" << std::endl;
 		return ;
 	}
+	
 	std::cout << "char: impossible" << std::endl;
 	std::cout << "int: impossible" << std::endl;
 	std::cout << "float: nanf" << std::endl;
